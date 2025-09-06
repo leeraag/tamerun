d79b1c0a74141dca8c3e5978e3ddafae90f3fb0d
alexander.kudashov <sasha.kudaschov2014@yandex.ru>
2025-09-06 10:41:22 +0300

    Add a framework for a Flask application to backend

---
+++ backend/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify
+from flasgger import Swagger
+
+app = Flask(__name__)
+swagger = Swagger(app, template = {
+    "swagger": "2.0",
+    "info": {
+        'title': 'API for tamerun-invest',
+        'version': '1.0',
+        'description': 'API for calculating the forecast of real estate ''
+                       'banking transactions'
+    }
+})
+
+"""
+Convert to json result
+:param key: key for json
+:param result: result calculations
+:param code: result code
+:return: result calculations in json format
+"""
+def to_result_json(key, result, code):
+    return jsonify({
+        key: result
+    }), code
+
+@app.route('/api/<int:first>/<int:second>', methods=['GET'])
+def calculation(first, second):
+    return to_result_json('result', first + second, 200)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
