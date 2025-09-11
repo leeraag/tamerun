72e004b0353d943d8fdcddd87fd7f5c1725498e4
alexander.kudashov <sasha.kudaschov2014@yandex.ru>
2025-09-11 21:05:17 +0300

    Update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
-import dns from 'dns'
 import react from '@vitejs/plugin-react'
 
-dns.setDefaultResultOrder('verbatim')
 
 // https://vite.dev/config/
 export default defineConfig({
