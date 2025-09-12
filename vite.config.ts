3360f36aea1e1dfabcc7b41409fd538f21abc629
Valeriia <valeriaagapova.2001@gmail.com>
2025-09-12 09:31:31 +0300

    turn off otimizeDeps

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-    base: './',
+  	plugins: [react()],
+	base: './',
 	server: {
-	  host: true,
-	  port: 443,
-	  hmr: false,
-	  allowedHosts: ['tamerun-invest.ru'],
+		host: true,
+		port: 443,
+		hmr: false,
+		allowedHosts: ['tamerun-invest.ru'],
 	},
+	optimizeDeps: {
+		noDiscovery: true,
+		include: []
+	}
 })
